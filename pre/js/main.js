a44cecf20755b67d854acefbbaa52450c1a80931
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-11 14:52:47 +0200

    Subida datos

--- pre/js/main.js
+++ pre/js/main.js
@@ -28,17 +28,17 @@ function main(error, distritosAux, data) {
     distritos = topojson.feature(distritosAux, distritosAux.objects.distritos);
 
     console.log(data);
+    console.log(distritos);
 
     ///HACEMOS EL JOIN
-    // muni.features.forEach(function(item) {
-    //     let join = data.filter(function(subItem) {
-    //         if(subItem.Municipios.substr(0,5) == item.properties.Codigo) {
-    //             return subItem;
-    //         }
-    //     });
-    //     join = join[0];
-    //     item.data = join;
-    // });
+    distritos.features.forEach(function(item) {
+        let join = data.filter(function(subItem) {
+            if(subItem.municipio_distrito.trim() == item.properties.NOMBRE) {
+                return subItem;
+            }
+        });
+        item.data = join;
+    });
 
     projection = d3_composite.geoConicConformalSpain().scale(2000).fitSize([width,height], distritos);
     path = d3.geoPath(projection);
@@ -52,98 +52,117 @@ function main(error, distritosAux, data) {
         .style('opacity', '1')
         .style('fill', function(d) {
             if(d.data) {
-                if (d.data.porc_envejecido != 'NA') {
-                    let color = '';
-                    let env = +d.data.porc_envejecido.replace(',','.');
-                    let total = +d.data.total;
+                let color = '';
 
-                    if ( total < 1000) {
-                        if (env < 15) {
-                            color = '#e8e8e8';
-                        } else if (env >= 15 && env < 30) {
-                            color = '#b5c0da';
-                        } else {
-                            color = '#6c83b5';
-                        }
-                    } else if ( total >= 1000 && total < 20000) {
-                        if (env < 15) {
-                            color = '#b8d6be';
-                        } else if (env >= 15 && env < 30) {
-                            color = '#8fb2b3';
-                        } else {
-                            color = '#567994';
-                        }
+                let renta = +d.data[0].renta_neta_media_persona;
+                let tasa_covid = +d.data[0].tasa_incidencia_acumulada_total;
+
+                if(tasa_covid < 141.5) {
+
+                    if (renta < 14575) {
+                        color = '#e8e8e8';
+                    } else if (renta >= 14575 & renta < 18590) {
+                        color = '#bddede';
+                    } else if (renta >= 18590 & renta < 23325) {
+                        color = '#8ed4d4';
                     } else {
-                        if (env < 15) {
-                            color = '#73ae7f';
-                        } else if (env >= 15 && env < 30) {
-                            color = '#5a9178';
-                        } else {
-                            color = '#2b5a5b';
-                        }
-                    }
+                        color = '#5ac8c8';
+                    }   
 
-                    return color;
+                } else if (tasa_covid >= 141.5 & tasa_covid < 162.7) {
 
+                    if (renta < 14575) {
+                        color = '#dabdd4';
+                    } else if (renta >= 14575 & renta < 18590) {
+                        color = '#bdbdd4';
+                    } else if (renta >= 18590 & renta < 23325) {
+                        color = '#8ebdd4';
+                    } else {
+                        color = '#5abdc8';
+                    } 
+                
+                } else if(tasa_covid >= 162.7 & tasa_covid < 184.2) {
+
+                    if (renta < 14575) {
+                        color = '#cc92c1';
+                    } else if (renta >= 14575 & renta < 18590) {
+                        color = '#bd92c1';
+                    } else if (renta >= 18590 & renta < 23325) {
+                        color = '#8e92c1';
+                    } else {
+                        color = '#5a92c1';
+                    } 
 
                 } else {
-                    return '#ccc';
-                }                
+
+                    if (renta < 14575) {
+                        color = '#be64ac';
+                    } else if (renta >= 14575 & renta < 18590) {
+                        color = '#bd64ac';
+                    } else if (renta >= 18590 & renta < 23325) {
+                        color = '#8e64ac';
+                    } else {
+                        color = '#5a64ac';
+                    } 
+
+                }
+
+                return color;
             } else {
                 return '#ccc';
             }            
         })
         .attr("d", path);
 
-        mapLayer2.selectAll(".dist2")
-            .data(distritos.features)
-            .enter()
-            .append("path")
-            .attr("class", "dist2")
-            .style('stroke','none')
-            .style('opacity', '1')
-            .style('fill', function(d) {
-                if(d.data) {
-                    if (d.data.porc_envejecido != 'NA') {
-                        let color = '';
-                        let env = +d.data.porc_envejecido.replace(',','.');
-                        let total = +d.data.total;
+        // mapLayer2.selectAll(".dist2")
+        //     .data(distritos.features)
+        //     .enter()
+        //     .append("path")
+        //     .attr("class", "dist2")
+        //     .style('stroke','none')
+        //     .style('opacity', '1')
+        //     .style('fill', function(d) {
+        //         if(d.data) {
+        //             if (d.data.porc_envejecido != 'NA') {
+        //                 let color = '';
+        //                 let env = +d.data.porc_envejecido.replace(',','.');
+        //                 let total = +d.data.total;
 
-                        if ( total < 1000) {
-                            if (env < 15) {
-                                color = '#e8e8e8';
-                            } else if (env >= 15 && env < 30) {
-                                color = '#b5c0da';
-                            } else {
-                                color = '#6c83b5';
-                            }
-                        } else if ( total >= 1000 && total < 20000) {
-                            if (env < 15) {
-                                color = '#b8d6be';
-                            } else if (env >= 15 && env < 30) {
-                                color = '#8fb2b3';
-                            } else {
-                                color = '#567994';
-                            }
-                        } else {
-                            if (env < 15) {
-                                color = '#73ae7f';
-                            } else if (env >= 15 && env < 30) {
-                                color = '#5a9178';
-                            } else {
-                                color = '#2b5a5b';
-                            }
-                        }
+        //                 if ( total < 1000) {
+        //                     if (env < 15) {
+        //                         color = '#e8e8e8';
+        //                     } else if (env >= 15 && env < 30) {
+        //                         color = '#b5c0da';
+        //                     } else {
+        //                         color = '#6c83b5';
+        //                     }
+        //                 } else if ( total >= 1000 && total < 20000) {
+        //                     if (env < 15) {
+        //                         color = '#b8d6be';
+        //                     } else if (env >= 15 && env < 30) {
+        //                         color = '#8fb2b3';
+        //                     } else {
+        //                         color = '#567994';
+        //                     }
+        //                 } else {
+        //                     if (env < 15) {
+        //                         color = '#73ae7f';
+        //                     } else if (env >= 15 && env < 30) {
+        //                         color = '#5a9178';
+        //                     } else {
+        //                         color = '#2b5a5b';
+        //                     }
+        //                 }
 
-                        return color;
+        //                 return color;
 
 
-                    } else {
-                        return '#ccc';
-                    }                
-                } else {
-                    return '#ccc';
-                }            
-            })
-            .attr("d", path);
+        //             } else {
+        //                 return '#ccc';
+        //             }                
+        //         } else {
+        //             return '#ccc';
+        //         }            
+        //     })
+        //     .attr("d", path);
 }
